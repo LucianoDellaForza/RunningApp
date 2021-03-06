2e510feac7ec0b5c5b8eb1767797509f8fae1b57
Luka <krivacevicluka@gmail.com>
2021-03-06 19:12:09 +0100

    Setting up Navigation

--- app/src/main/java/com/chelios/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/chelios/runningapp/ui/MainActivity.kt
@@ -2,16 +2,33 @@ package com.chelios.runningapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.chelios.runningapp.R
 import com.chelios.runningapp.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-@AndroidEntryPoint
+@AndroidEntryPoint  //this activity holds fragment in which i inject stuff, so i need this annotation
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        //Bottom navigation to not show in SetupFragment and TrackingFragment
+        navHostFragment.findNavController()
+                .addOnDestinationChangedListener { _, destination, _ -> //triggered whenever destination changes with navigation components
+                    when (destination.id) {
+                        R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                            bottomNavigationView.visibility = View.VISIBLE
+                        else -> bottomNavigationView.visibility = View.GONE
+                    }
+                }
     }
 }
--- app/src/main/java/com/chelios/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/chelios/runningapp/ui/fragments/RunFragment.kt
@@ -1,14 +1,25 @@
 package com.chelios.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.chelios.runningapp.R
 import com.chelios.runningapp.ui.viewModels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
 
     private val viewModel: MainViewModel by viewModels()    //dagger manages ViewModel Factories behind the scenes
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
