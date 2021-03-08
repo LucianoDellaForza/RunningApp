579112a13b3380f184ef026f669c8cf37f54c0ff
Luka <krivacevicluka@gmail.com>
2021-03-08 20:40:16 +0100

    Handling the Map's Lifecycle

--- app/src/main/java/com/chelios/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/chelios/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,14 +1,62 @@
 package com.chelios.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.chelios.runningapp.R
 import com.chelios.runningapp.ui.viewModels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()    //dagger manages ViewModel Factories behind the scenes
+
+    private var map: GoogleMap? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    /** mapView has its own lifecycle that I need to handle **/
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
 
 }
